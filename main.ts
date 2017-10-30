894d3afc2d5ea63245d35bf9504d65ec48ccbdc3
Yuriy Zubaha <yuzub@ua.fm>
2017-10-31 01:57:31 +0200

    Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-alert('ok');
+// alert('ok');
+import { Observable } from 'rxjs';
+
+let numbers = [1, 5, 10];
+// let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        } else {
+            observer.complete();
+        }
+    }
+
+    produceValue();
+
+});
+
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log('complete')
+);
---
+++ main1.ts
@@ -0,0 +1,42 @@
+// alert('ok');
+import { Observable, Observer } from 'rxjs';
+
+let numbers = [1, 5, 10];
+// let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+    
+    for (let n of numbers) {
+        if (n === 5) {
+            observer.error('Something went wrong!');
+        }
+        observer.next(n);
+    }
+
+    observer.complete();
+
+});
+
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log('complete')
+);
+
+class MyObserver implements Observer<number> {
+
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+
+}
+
+// source.subscribe(new MyObserver());
+// source.subscribe(new MyObserver());
